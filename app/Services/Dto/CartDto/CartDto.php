80a7ca023b3ec19ecf833a6d62d6109ae94fa8fe
armanbadasyan <arman.badasyan.111@gmail.com>
2023-12-29 13:35:50 +0400

    Zadacha

---
+++ app/Services/Dto/CartDto/CartDto.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Services\Dto\CartDto;
+
+
+use Spatie\LaravelData\Data;
+use App\Http\Requests\CartRequest\CartRequest;
+
+
+class CartDto extends Data
+{
+public  int $product_id;
+
+    public static function fromRequest(CartRequest $request):CartDto
+    {
+
+        return self::from([
+            'product_id' => $request->getProductId(),
+
+        ]);
+    }
+}
